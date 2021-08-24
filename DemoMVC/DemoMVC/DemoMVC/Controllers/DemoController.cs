6f8d7bff02d4fd8570b03a735d12d64b482676c6
Admin <Admin@DESKTOP-S6HDA9U>
2021-08-24 15:25:21 -0700

    baitap2

---
+++ DemoMVC/DemoMVC/DemoMVC/Controllers/DemoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hello()
+        {
+            return View();
+        }
+        public ActionResult GiaiPTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1(double heSoA, double heSoB)
+        {
+            double x = gpt.GiaiPhuongTrinhBacNhat(heSoA, heSoB);
+            ViewBag.nghiemPT = x;
+            return View();
+        }
+    }
+}
---
+++ DemoMVC/DemoMVC/DemoMVC/Models/GiaiPhuongTrinh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class GiaiPhuongTrinh
+    {
+        public double GiaiPhuongTrinhBacNhat(double a, double b)
+        {
+            double x;
+            x = -b / a;
+            return x;
+        }
+    }
+}
